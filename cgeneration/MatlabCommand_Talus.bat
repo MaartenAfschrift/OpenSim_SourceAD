8af95eb8d96388b28c23d7b7882fb874a2c6fc30
Maarten Afschrift <maarten.afschrift@kuleuven.be>
2020-05-19 16:57:45 +0200

    Added external functions to actuate tibia and calcaneus

---
+++ cgeneration/MatlabCommand_Talus.bat
@@ -0,0 +1,5 @@
+cd C:\opensim-ad-core-source\cgeneration\SimExo_3D_talus
+"C:\Program Files\MATLAB\R2018b\bin\matlab.exe" -nodisplay -nosplash -nodesktop -r "generateMain; exit"
+cd C:\opensim-ad-core-source\cgeneration\SimExo_3D_talus_out
+"C:\Program Files\MATLAB\R2018b\bin\matlab.exe" -nodisplay -nosplash -nodesktop -r "generateMain; exit"
+PAUSE
